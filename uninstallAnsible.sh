ae09cc0013c982790e4d752bd0581609c512fdf7
l50 <jayson.e.grace@gmail.com>
2018-12-04 14:19:34 -0800

    Add autoremove functionality for entries in bash_rc and bash_profile for uninstallAnsible.sh

--- uninstallAnsible.sh
+++ uninstallAnsible.sh
@@ -13,6 +13,13 @@ rm -rf "$HOME/.pyenv"
 sudo rm -rf /etc/ansible
 ansibleBins=('ansible' 'ansible-connection' 'ansible-console' 'ansible-doc' 'ansible-galaxy' 'ansible-playbook' 'ansible-pull' 'ansible-vault')
 for ((i=0; i<${#ansibleBins[*]}; i++)); do
-  sudo rm /usr/local/bin/${ansibleBins[i]}
+    sudo rm /usr/local/bin/${ansibleBins[i]}
 done
-echo 'remove the entries from ~/.bashrc && ~/.bash_profile or ~/.zshrc'
+
+echo 'Removing the entries from $HOME/.bashrc and $HOME/.bash_profile'
+if grep -q 'pyenv virtualenv-init' $HOME/.bash_profile; then
+    sed -i '$ d' $HOME/.bashrc
+    for ((i=0; i<3; i++)); do
+        sed -i '$ d' $HOME/.bash_profile
+    done
+fi
